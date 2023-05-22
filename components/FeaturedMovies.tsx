876b05dc39b1f0c6a0698c8d19ebfa332ab0e05c
adetimmy <ogunleademola@gmail.com>
2023-05-22 01:37:50 +0100

    setting up my api and key

--- components/FeaturedMovies.tsx
+++ components/FeaturedMovies.tsx
@@ -10,30 +10,33 @@ interface TitleProps{
 
 const FeaturedMovies = ({title}:TitleProps) => {
 
-  const scrollRef = React.useRef<HTMLDivElement>(null);
+  const scrollRef = React.useRef<HTMLDivElement | null>(null);
+  const [seeMore, setSeeMore] = React.useState<Boolean>(false)
 
 const scroll = (direction: string) => {
-  const current = scrollRef.current;
-
-  if (current) {
-    current.scrollLeft += direction === 'left' ? -600 : 600;
+ 
+  if (scrollRef.current) {
+    scrollRef.current.scrollTo({
+      left: direction === 'left' ? scrollRef.current.scrollLeft - 300 : scrollRef.current.scrollLeft + 300,
+      behavior: 'smooth',
+    });
   }
-};
+}
 
   return (
     <>
     <div className="flex justify-between">
         <h1 className="font-bold dark:text-gray-600 md:text-2xl">{title}</h1>
-        <p className="text-[#BE123C] text-xs sm:text-sm md:text-base flex items-center gap-1.5 cursor-pointer">See more<RiArrowRightSLine/></p>
+        <p className="text-[#BE123C] text-xs sm:text-sm md:text-base flex items-center gap-1.5 cursor-pointer" onClick={() => setSeeMore(prev => !prev)}>{seeMore? "See less" : "See more"}<RiArrowRightSLine/></p>
     </div>
 
 
-    <div className="flex item-center relative">
+    <div className="flex item-center  relative">
     
-      <SlArrowLeft fontSize={23} className=" cursor-pointer absolute top-[40%] left-0 text-black dark:text-white font-extrabold" onClick={() => scroll('left')}/>
+      <SlArrowLeft fontSize={23} className={`cursor-pointer absolute top-[40%] left-0 text-black dark:text-white font-extrabold ${seeMore? 'hidden' :'inline-block' }`} onClick={() => scroll('left')}/>
 
       <div className="overflow-x-scroll container scroll flex min-w-full px-12 " ref={scrollRef}>
-        <div className="flex gap-12 ">
+        <div className={`flex gap-12 ${seeMore? 'flex-wrap' :'flex-nowrap' }`}>
           <Card/>
           <Card/>
           <Card/>
@@ -63,7 +66,7 @@ const scroll = (direction: string) => {
         </div>  
       </div>
     
-      <SlArrowRight fontSize={23} className="font-extrabold absolute top-[40%] right-0 text-black dark:text-white cursor-pointer" onClick={() => scroll('right')} /> 
+      <SlArrowRight fontSize={23} className={`cursor-pointer absolute top-[40%] right-0 text-black dark:text-white font-extrabold ${seeMore? 'hidden' :'inline-block'}` } onClick={() => scroll('right')} /> 
     </div>
     
     </>
---
+++ components/MovieList.ts
@@ -0,0 +1,9 @@
+export const movies = {
+    movie: {
+        HeaderMovies:'https://api.themoviedb.org/3/trending/all/day?api_key=cf0697c3093ede8a9be759954df1540c',
+        TopRatedMovies:'https://api.themoviedb.org/3/movie/top_rated?api_key=cf0697c3093ede8a9be759954df1540c',
+        NewArrival:'https://api.themoviedb.org/3/movie/popular?api_key=cf0697c3093ede8a9be759954df1540c',
+        ExclusiveVideos:'https://api.themoviedb.org/3/movie/upcoming?api_key=cf0697c3093ede8a9be759954df1540c',
+        FeaturedCast:'https://api.themoviedb.org/3/person/popular?api_key=cf0697c3093ede8a9be759954df1540c',
+  }
+}
